4dbd02fd19f92afaeecd8b90fa7fd808f613c844
faizal-siddiqui <faizalsidp@gmail.com>
2023-02-15 03:29:31 +0530

    User Model Created

---
+++ Backend/Model/User.Model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true },
+  dob: { type: String, required: false },
+  location: { type: String, required: false },
+});
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = {
+  userModel,
+};

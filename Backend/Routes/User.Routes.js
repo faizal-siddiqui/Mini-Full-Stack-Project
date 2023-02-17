c5b2a07e612e98bbbf80b39fd60a1bc758ee542d
faizal-siddiqui <faizalsidp@gmail.com>
2023-02-17 18:44:05 +0530

    updated User Routes

--- Backend/Routes/User.Routes.js
+++ Backend/Routes/User.Routes.js
@@ -1,106 +1,39 @@
 const express = require("express");
 const { userModel } = require("../Model/User.Model");
 const jwt = require("jsonwebtoken");
+const { Authenticator } = require("../Middlewares/Authenticator.middleware");
+const { Validator } = require("../Middlewares/Validator.middleware");
+const { register } = require("../Controller/User/register.controller");
+const { login } = require("../Controller/User/login.controller");
+const { getAllUser } = require("../Controller/User/getAllUser.controller");
+const { getSingeUser } = require("../Controller/User/getSingleUser.controller");
+const { updateUser } = require("../Controller/User/updateUser.controller");
+const { deleteUser } = require("../Controller/User/deleteUser.controller");
+const { userLogger } = require("../Middlewares/userLogger.middleware");
+const { verifyToken } = require("../Middlewares/verifyToken.middleware");
+const {
+  checkHashPassword,
+} = require("../Middlewares/checkHashPassword.middleware");
+
 const userRouter = express.Router();
 
 // SIGNUP
-userRouter.post("/register", async (req, res) => {
-  const userDetails = req.body;
-  try {
-    const newUser = new userModel(userDetails);
-    await newUser.save();
-    res.send({ msg: "Accounts Created" });
-  } catch (err) {
-    res.send({ err: err.message });
-  }
-});
-
+userRouter.post("/register", register);
 // LOGIN
-userRouter.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const data = await userModel.find({ email, password });
-    if (data.length > 0) {
-      const token = jwt.sign(
-        {
-          exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
-          data: "faizal",
-        },
-        "faizal"
-      );
-      res.send({ msg: "Accounts Created", token: token });
-    } else {
-      res.send({ msg: "Session Expired Login Again" });
-    }
-  } catch (err) {
-    res.send({ err: err.message });
-  }
-});
-
+userRouter.post(
+  "/login",
+  [Authenticator, checkHashPassword, userLogger],
+  login
+);
 // GET ALL USERS
-userRouter.get("/", async (req, res) => {
-  const token = req.headers.authorization;
-  jwt.verify(token, "faizal", async (err, decoded) => {
-    try {
-      if (decoded) {
-        const users = await userModel.find();
-        res.send(users);
-      } else {
-        res.send({ msg: "Session Expired Login Again" });
-      }
-    } catch (err) {
-      res.send({ err: err.message });
-    }
-  });
-});
-
+userRouter.get("/", verifyToken, getAllUser);
 // GET Single USERS
-userRouter.get("/:id", async (req, res) => {
-  const token = req.headers.authorization;
-  jwt.verify(token, "faizal", async (err, decoded) => {
-    try {
-      if (decoded) {
-        const user = await userModel.find({ _id: req.params.id });
-        res.send(user);
-      } else {
-        res.send({ msg: "Session Expired Login Again" });
-      }
-    } catch (err) {
-      res.send({ err: err.message });
-    }
-  });
-});
-
+userRouter.get("/:id", verifyToken, getSingeUser);
 // PATCH Single USERS
-userRouter.patch("/:id", async (req, res) => {
-  const token = req.headers.authorization;
-  jwt.verify(token, "faizal", async (err, decoded) => {
-    try {
-      if (decoded) {
-        await userModel.findByIdAndUpdate({ _id: req.params.id }, req.body);
-        res.send("Users Details Updated");
-      } else {
-        res.send({ msg: "Session Expired Login Again" });
-      }
-    } catch (err) {
-      res.send({ err: err.message });
-    }
-  });
-});
-
+userRouter.patch("/:id", [verifyToken, Validator], updateUser);
 // DELETE Single USERS
-userRouter.delete("/:id", async (req, res) => {
-  const token = req.headers.authorization;
-  jwt.verify(token, "faizal", async (err, decoded) => {
-    try {
-      if (decoded) {
-        await userModel.findByIdAndDelete({ _id: req.params.id });
-        res.send("Users Details Deleted");
-      } else {
-        res.send({ msg: "Session Expired Login Again" });
-      }
-    } catch (err) {
-      res.send({ err: err.message });
-    }
-  });
-});
+userRouter.delete("/:id", [verifyToken, Validator], deleteUser);
+
+module.exports = {
+  userRouter,
+};

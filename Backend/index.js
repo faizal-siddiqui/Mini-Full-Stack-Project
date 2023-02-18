1ccde9d3b0eff0b682d9ccdc37e352a135dd17cd
faizal-siddiqui <faizalsidp@gmail.com>
2023-02-18 20:03:40 +0530

    completed backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const { connection } = require("./Configs/db");
 const { userRouter } = require("./Routes/User.Routes");
-
+const cors = require("cors");
+const { projectRouter } = require("./Routes/Project.Routes");
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/users", userRouter);
+app.use("/projects", projectRouter);
 // console.log(process.env.PORT);
-
 app.get("/", (req, res) => {
   res.send("Home Page");
 });

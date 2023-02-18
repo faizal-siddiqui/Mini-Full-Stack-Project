0d7720f1f317f3bf18e91ae2bdc9221f56b27dd7
faizal-siddiqui <faizalsidp@gmail.com>
2023-02-18 20:02:10 +0530

    added verify Token middleware

--- Backend/Middlewares/verifyToken.middleware.js
+++ Backend/Middlewares/verifyToken.middleware.js
@@ -10,7 +10,7 @@ const verifyToken = (req, res, next) => {
         req.body.userId = decoded.userId;
         next();
       } else {
-        res.send({ msg: "Session Expired Login Again" });
+        res.statusCode(500).send({ msg: "Session Expired Login Again" });
       }
     });
   } else {
